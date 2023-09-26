5457da193f53ac18c7df4cccc39225ca5a653118
aasim-khan-989 <119357766+aasim-khan-989@users.noreply.github.com>
2023-09-26 08:44:45 +0530

    Added programs of Number data Type and Math Library

---
+++ 08_Dates.js
@@ -0,0 +1,36 @@
+// Date is an object is Js
+
+
+// console.log(Date)
+
+let myDate = new Date()
+
+/*
+
+
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toJSON())
+console.log(myDate.toDateString())
+*/
+
+// let myCreatedDate = new Date(2023,0,3,5,30)
+// console.log(myCreatedDate.toString())
+//  myCreatedDate = new Date("2023-01-3")
+//  console.log(myCreatedDate.toString())
+
+
+// Time Stamp
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp.toString())
+// console.log(myDate.getTime())
+
+
+
+
+
+
+
+
