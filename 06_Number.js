8d35ce395e46d831f3073c0c87ad7e39bc3d3e50
aasim-khan-989 <119357766+aasim-khan-989@users.noreply.github.com>
2023-09-29 01:32:14 +0530

    Arras in JavaScript

--- 06_Number.js
+++ 06_Number.js
@@ -1,17 +1,17 @@
 //Two ways to  declare Number data type
 
-/*const num = 25
+const num = 25
 const num2 = new Number(25);
 console.log(num)
 console.log(typeof(num))  
 console.log(num2)
-console.log(typeof(num2)) */
+console.log(typeof(num2)) 
 
 // Import maths of number datatype
 
-const num = 100.3678
-console.log(num.toString())
-console.log(num.toFixed(2))
+const num3 = 100.3678
+console.log(num3.toString())
+console.log(num3.toFixed(2))
 /* Note : The toPrecision() method formats a number to a specified length.
 A decimal point and nulls are added (if needed), to create the specified length.*/
 console.log(num.toPrecision(5)) 
